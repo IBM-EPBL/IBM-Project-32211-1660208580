098c0273af2f518f4d2496b030d31cd7d79dc1e7
Navinkumar369 <navinkumar.it.2001@gmail.com>
2022-10-27 10:23:05 +0530

    navin assignment2

---
+++ Assignment/Navinkumar/Assingment2/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return "<h2>Developer : P.Navinkumar</h2>"
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
